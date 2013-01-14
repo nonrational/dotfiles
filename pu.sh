31cc69b9c8a51ad4d2d0952d8811491505c5481b
Alan Norton <alan@betterment.com>
2013-01-14 15:35:01 -0500

    add exclusion list for dot file copying

--- pu.sh
+++ pu.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-# assume this in run in the .dotfiles checkout
-DOTS="$HOME/.dotfiles"
-if [ "$1" != "" ]; then
-    DOTS="$1"
-fi
-
-if [ ! -d $DOTS ]; then
-    echo "$DOTS does not exist! Aborting..."
-    exit 1
-fi
-
 force_delete=0
-
+DOTS="$HOME/.dotfiles"
 uname="`uname`"
 host="`uname -n | sed -e 's/\.local//g'`";
 
 # make some options require arguments
-# set -- $(getopt abf: "$@")
+# set -- $(getopt d: "$@")
 
 while [ $# -gt 0 ]
 do
     case "$1" in
     (-f) force_delete=1; shift;;
-    (--) shift; break;;
+    # (-d) shift; break;;
     (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
     (*)  break;;
     esac
     shift
 done
+
+if [ ! -d $DOTS ]; then
+    echo "$DOTS does not exist! Aborting..."
+    exit 1
+fi
 
 linky(){
     file=$1
@@ -59,14 +53,25 @@ linky(){
     fi
 }
 
+exclusion_patterns=(".git" ".gitignore" ".DS_Store" "." ".." ".AppleDouble");
+exclusion_list="${exclusion_patterns[@]}";
+
+should_copy() {
+    for e in $exclusion_list; do [[ "$e" == "$1" ]] && echo 1; done
+    echo 0
+}
+
 # Anything that starts with a dot should be linked as is.
 STANDARD_DOT_FILES=$DOTS/.*
 for sdf in $STANDARD_DOT_FILES; do
-    linky `basename $sdf`
+    bnf=`basename $sdf`;
+    if [[ `should_copy $bnf` == 0 ]]; then
+        linky $bnf
+    fi
 done
 
 # special cases - these don't start with a dot
-linky .ssh.config ~/.ssh/config
+linky ssh.config ~/.ssh/config
 linky "bin.$uname" ~/bin
 
 if [ "$uname" == "Darwin" ]; then
