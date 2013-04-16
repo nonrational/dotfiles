f5d5eb1f5b30a4b6c76e92e540797471e9980c65
Alan Norton <me@alannorton.com>
2013-04-16 18:20:15 -0400

    added some more defaults to osx prefs
    Former-commit-id: d3e82df0ee6e34020196e691c752e9a9dd29061d

--- etc/defaults.write.sh
+++ etc/defaults.write.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+defaults write com.apple.dashboard devmode YES
+
 defaults write com.apple.dock orientation left
+
+defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 
 killall Dock
 killall Finder
