fe2d609dd3928553d1eaf00db30fc342ab8790e2
Alan Norton <me@alannorton.com>
2015-08-10 11:26:14 -0400

    remind myself how to use my own tools

--- bin.Darwin/ec2named.rb
+++ bin.Darwin/ec2named.rb
@@ -6,6 +6,11 @@ app_name = ARGV[0]
 app_env  = ARGV[1]
 app_class = ARGV[2] || "pipeline"
 
+if !(app_name && app_env)
+  puts "ec2named wants app_name && app_env, in that order"
+  exit
+end
+
 di = JSON.parse(`aws ec2 describe-instances --filters "Name=tag:Name,Values=*#{app_env}.#{app_class}.#{app_name}*"`)
 
 puts di["Reservations"]
