da9989d1d7a7d71813ce60245cb2f4883b4f1db4
Alan Norton <me@alannorton.com>
2015-10-14 15:35:59 -0400

    atom

---
+++ .atom/compile-cache/coffee/f55c001218bab56807639b76ab1aba348a7517f1.js
@@ -0,0 +1,8 @@
+(function() {
+
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL25vcnRvbi8uYXRvbS9pbml0LmNvZmZlZSIKICBdLAogICJuYW1lcyI6IFtdLAogICJtYXBwaW5ncyI6ICJBQUFBO0FBQUE7QUFBQSIKfQ==
+
+//# sourceURL=/Users/norton/.atom/init.coffee
---
+++ .atom/packages/atom-espresso-soda-theme/README.md
@@ -0,0 +1,6 @@
+Espresso Soda Syntax Theme for Atom
+==============================
+
+Converted from the [TextMate and Sublime Text Theme](https://github.com/buymeasoda/soda-theme/) using the [TextMate bundle converter](http://atom.io/docs/latest/converting-a-text-mate-theme).
+
+![](https://raw.github.com/martinwolf/atom-espresso-soda-theme/master/theme.png)
