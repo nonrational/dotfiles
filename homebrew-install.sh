e22890210b2f6f37e6a10d4a969604e7f9b00f0a
Alan Norton <alan@mealpal.com>
2018-04-29 12:54:50 -0400

    all in on Brewfile

---
+++ homebrew-install.sh
@@ -0,0 +1,6 @@
+set -e
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# brew bundle
+
+
---
+++ iterm2-preferences.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp ./etc/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
