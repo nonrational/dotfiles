9e7e33ed7fae484d557ea5a693e36218cd98d12d
Alan Norton <alan@mealpal.com>
2019-07-27 12:38:13 -0400

    don't rely on .bootstrap
    Former-commit-id: f935e552ca18d03e1e88558a6e4c3fb23c58e351

--- install-everything.sh
+++ install-everything.sh
@@ -7,12 +7,14 @@ log_and_exec(){
 
 log_and_exec 00-install-osx-prefs.sh
 
-log_and_exec 01-install-homebrew.sh
+log_and_exec 01-install-development-headers.sh
+
+log_and_exec 10-install-homebrew.sh
 
 brew bundle
 
-log_and_exec 02-install-profile.sh
+log_and_exec 20-install-profile.sh
 
-log_and_exec 03-install-sublime-prefs.sh
+log_and_exec 30-install-sublime-prefs.sh
 
-log_and_exec 04-install-iterm2-preferences.sh
+log_and_exec 40-install-iterm2-preferences.sh
