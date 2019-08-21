89678392877f2c5e5fa0d1262a35a57f2ec44feb
Alan Norton <me@alannorton.com>
2019-08-21 11:01:59 -0400

    diff order
    Former-commit-id: 3105ad582c8519898b19cd2c95e87110a1d7eba6

--- install-everything.sh
+++ install-everything.sh
@@ -5,16 +5,14 @@ log_and_exec(){
     sh $1
 }
 
-# log_and_exec 00-install-osx-prefs.sh
-
+log_and_exec 00-install-osx-prefs.sh
 log_and_exec 01-install-development-headers.sh
-
 log_and_exec 10-install-homebrew.sh
 
 brew bundle
 
 log_and_exec 20-install-profile.sh
-
 log_and_exec 30-install-sublime-prefs.sh
-
+log_and_exec 31-install-atom-prefs.sh
 log_and_exec 40-install-iterm2-preferences.sh
+log_and_exec 50-install-karabiner-preferences.sh
