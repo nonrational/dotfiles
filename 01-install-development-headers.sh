aa7afb155f6889d1a5857d588f328625309210aa
Alan Norton <me@alannorton.com>
2019-09-03 12:53:56 -0400

    timesync'd

--- 01-install-development-headers.sh
+++ 01-install-development-headers.sh
@@ -4,3 +4,6 @@ if [ -e /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS
   echo 'Installing Mojave Development Headers'
   sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 fi
+
+sudo touch /var/db/ntp-kod
+sudo chmod 666 /var/db/ntp-kod
