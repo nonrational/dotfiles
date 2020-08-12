a85d8ef30abd6a2af78ad5e1ebe44f94d922b490
Alan Norton <git@nonration.al>
2020-08-11 22:55:21 -0400

    shellcheck.
    Former-commit-id: f6f1c5a01e6989384495f9d816c56d84ec15b6f1

--- 20-install-profile.sh
+++ 20-install-profile.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
 force_delete=0
-apply_root=0
 
 DOTS="$PWD"
-uname="`uname`"
-host="`uname -n | sed -e 's/\.local//g'`";
-
-# make some options require arguments
-# set -- $(getopt d: "$@")
+uname="$(uname)"
 
 while [ $# -gt 0 ]
 do
     case "$1" in
     (-f) force_delete=1; shift;;
-    (-r) apply_root=1; shift;;
     (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
     (*)  break;;
     esac
     shift
 done
 
-if [ ! -d $DOTS ]; then
+if [ ! -d "$DOTS" ]; then
     echo "$DOTS does not exist! Aborting..."
     exit 1
 fi
 
-linky(){
-    file=$1
+symlink(){
+    file="$1"
     src="$DOTS/$file"
 
     if [ ! -e "$src" ]; then
@@ -37,51 +31,38 @@ linky(){
 
     trg=""
     if [ "$2" == "" ]; then
-        trg=$HOME/$file
+        trg="$HOME/$file"
     else
-        trg=$2
+        trg="$2"
     fi
 
     if [ -e "$trg" ]; then
         if [ $force_delete == 1 ]; then
-            rm -rf $trg
-            ln -sv $src $trg
+            rm -rf "$trg"
+            ln -sv "$src" "$trg"
         else
             echo "[error] unable to push $src; $trg exists."
         fi
     else
-        mkdir -p "`dirname $trg`"
-        ln -fsv $src $trg
+        mkdir -p "$(dirname "$trg")"
+        ln -fsv "$src" "$trg"
     fi
 }
 
 exclusion_patterns=(".git" ".gitignore" ".DS_Store" "." ".." ".AppleDouble");
-exclusion_list="${exclusion_patterns[@]}";
+exclusion_list=( "${exclusion_patterns[@]}" );
 
-should_copy() {
-    for e in $exclusion_list; do [[ "$e" == "$1" ]] && echo 1; done
-    echo 0
+should_symlink() {
+    for e in "${exclusion_list[@]}"; do [[ "$e" == "$1" ]] && echo "1"; done
+    echo "0"
 }
 
 # Anything that starts with a dot should be linked as is.
-STANDARD_DOT_FILES=$DOTS/.*
-for sdf in $STANDARD_DOT_FILES; do
-    bnf=`basename $sdf`;
-    if [[ `should_copy $bnf` == 0 ]]; then
-        linky $bnf
-    fi
+STANDARD_DOT_FILES=( "$DOTS/.*" )
+for dot_file_path in "${STANDARD_DOT_FILES[@]}"; do
+    dot_file_basename=$(basename "$dot_file_path");
+    [[ "$(should_symlink "$dot_file_basename")" == "0" ]] && symlink "$dot_file_basename"
 done
 
 # special cases - these don't start with a dot
-linky "bin.$uname" ~/bin
-
-# root configurations
-if [[ $apply_root == 1 ]]; then
-    sudo ln -sfv $DOTS/root.profile /var/root/.profile
-fi
-
-# if [ "$uname" == "Darwin" ]; then
-#     echo "Linking OS X Specific Addons ... ";
-# elif [ "$uname" == "Linux" ]; then
-#     echo "Linking Linux Specific Addons ... "
-# fi
+symlink "bin.$uname" ~/bin
