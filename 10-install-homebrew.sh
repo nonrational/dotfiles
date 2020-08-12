63e05aba15c5b776898147b4921253ad73300266
Alan Norton <git@nonration.al>
2020-08-11 22:36:33 -0400

    duh. no reason to not set osx prefs _at the end_.
    Former-commit-id: 9b15fdc3a146708b63c7d72fd6dfc491593ba5fd

--- 10-install-homebrew.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# brew bundle
-
-
--- 31-install-atom-prefs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/nonrational/atom $HOME/.atom
-
-cd $HOME/.atom && ./script/apm-restore
