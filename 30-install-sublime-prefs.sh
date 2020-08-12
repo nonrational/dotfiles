93a115d897239a859bf379ba45983879d70a8aa0
Alan Norton <git@nonration.al>
2020-08-12 14:12:23 -0400

    makefile is easier to read, cleanup

--- 30-install-sublime-prefs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/nonrational/sublime3 $HOME/.sublime3 || echo 'got it'
-rm -rf $HOME/Library/Application\ Support/Sublime\ Text\ 3
-ln -s $HOME/.sublime3 $HOME/Library/Application\ Support/Sublime\ Text\ 3
--- 40-install-iterm2-preferences.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp ./etc/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
