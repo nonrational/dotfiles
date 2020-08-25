054e5b746d3b3f4dfd4bfb5d1005420a24485532
Alan Norton <git@nonration.al>
2020-08-25 10:40:56 -0400

    git broom without git up
    Former-commit-id: 88f403a61ff6e409d424dfb539c7d79ea5afa608

---
+++ bin.Darwin/git-broom.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+remote_branches = `git branch --remote | grep "origin" | grep -v "HEAD" | cut -c10- | egrep -v "^master$\|^main$"`.split("\n")
+local_branches = `git branch | grep -v "HEAD" | cut -c3- | egrep -v "^master$\|^main$"`.split("\n")
+
+local_branches.each do |lb|
+  unless remote_branches.include? lb
+    print "Delete #{lb}? (y/n): "
+    puts `git branch -D #{lb}` if /\Ay/i =~ gets.chomp
+  end
+end
