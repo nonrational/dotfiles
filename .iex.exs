1d411d4dd4ae5efe4c1692725ca55a1c642c4502
Alan Norton <git@nonration.al>
2020-09-04 12:38:26 -0400

    parallel elixir helpers
    Former-commit-id: b9f8b3e29b6a5826149a1c8d1348839a9419dd9c

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,13 @@
-IO.puts("hello world")
+# parallel
+import FM.Factory
 
-value = 13
+defmodule TT do
+  def reload_emails do
+    [Mix.Tasks.Fm.Emails, FMWeb.EmailView, FMWeb.Email, Mix.Tasks.Fm.Emails]
+    |> Enum.each(&r(&1))
+  end
+
+  def test_all_emails do
+    Mix.Task.run("fm.emails")
+  end
+end
