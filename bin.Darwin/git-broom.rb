54e0735168997a854abc87edf6c22fa7a26ac283
Alan Norton <git@nonration.al>
2020-09-08 13:28:46 -0400

    fetch first
    Former-commit-id: cf5229fb8dc235cc48a714d28ff7a85397cbc646

--- bin.Darwin/git-broom.rb
+++ bin.Darwin/git-broom.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+`git fetch origin --prune --tags -f`
+
 remote_branches = `git branch --remote | grep "origin" | grep -v "HEAD" | cut -c10- | egrep -v "^master$\|^main$"`.split("\n")
 local_branches = `git branch | grep -v "HEAD" | cut -c3- | egrep -v "^master$\|^main$"`.split("\n")
 
