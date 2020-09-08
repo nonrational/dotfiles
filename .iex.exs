8667c17e7cdadebd2439aa501bb8e1e2b7635890
Brian Muller <bamuller@gmail.com>
2020-09-08 12:57:41 -0400

    Update .iex.exs (#4)
    Former-commit-id: 40707e659e7b08d656e92c8c7c695351e8b51c4d

--- .iex.exs
+++ .iex.exs
@@ -4,7 +4,7 @@ import FM.Factory
 defmodule TT do
   def reload_emails do
     [Mix.Tasks.Fm.Emails, FMWeb.EmailView, FMWeb.Email, Mix.Tasks.Fm.Emails]
-    |> Enum.each(&r(&1))
+    |> Enum.each(&r/1)
   end
 
   def test_all_emails do
