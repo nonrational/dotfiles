36029ac539014c2bc3363682840011be92cd22e5
Alan Norton <git@nonration.al>
2020-09-19 12:15:32 -0400

    incremental
    Former-commit-id: 625b4bfa7a78b16ed6155ffc3a845992d79602ce

--- .iex.exs
+++ .iex.exs
@@ -1,13 +1,2 @@
 # parallel
-import FM.Factory
-
-defmodule TT do
-  def reload_emails do
-    [Mix.Tasks.Fm.Emails, FMWeb.EmailView, FMWeb.Email, Mix.Tasks.Fm.Emails]
-    |> Enum.each(&r/1)
-  end
-
-  def test_all_emails do
-    Mix.Task.run("fm.emails")
-  end
-end
+import_file("~/src/parallelize/.iex.exs")
